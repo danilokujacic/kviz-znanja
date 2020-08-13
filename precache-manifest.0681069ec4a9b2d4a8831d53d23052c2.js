5a5bc4e2aba96bae09bb947cef1bb02d05b64550
Danilo <danilo.kujacic@bild-studio.net>
2020-08-13 15:05:28 +0200

    Updates

---
+++ precache-manifest.0681069ec4a9b2d4a8831d53d23052c2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386f652f40d89ff14bf19e0cb5055872",
+    "url": "/kviz-znanja/index.html"
+  },
+  {
+    "revision": "283c167a0d0073e6674e",
+    "url": "/kviz-znanja/static/css/main.662334f9.chunk.css"
+  },
+  {
+    "revision": "7251fda0bd89226e64e5",
+    "url": "/kviz-znanja/static/js/2.6131203b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kviz-znanja/static/js/2.6131203b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283c167a0d0073e6674e",
+    "url": "/kviz-znanja/static/js/main.4acb4922.chunk.js"
+  },
+  {
+    "revision": "eb2e83f7768f5c36ef1c",
+    "url": "/kviz-znanja/static/js/runtime-main.dbc16757.js"
+  },
+  {
+    "revision": "3e78c53fd6fb5f74e9083a2bcd41095b",
+    "url": "/kviz-znanja/static/media/geography.3e78c53f.png"
+  },
+  {
+    "revision": "5a99e060b5cdabb2776dd05739fa8fe8",
+    "url": "/kviz-znanja/static/media/history.5a99e060.jpg"
+  },
+  {
+    "revision": "1376d7e46b243500420f933fbf5289d5",
+    "url": "/kviz-znanja/static/media/music.1376d7e4.jpg"
+  },
+  {
+    "revision": "8c6b6452da57d18dd77f03f5e1fab1fc",
+    "url": "/kviz-znanja/static/media/social.8c6b6452.png"
+  }
+]);
